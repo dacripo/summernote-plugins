0a52b450673c1ff0d68c64b756ea43fcf2dcfc8c
dacripo <dacripo@users.noreply.github.com>
2017-02-07 02:11:39 +0100

    Update summernote-emoji.js

--- summernote-emoji/summernote-emoji.js
+++ summernote-emoji/summernote-emoji.js
@@ -87,7 +87,7 @@
             function addimg(value) {
                 var img = $('<iframe frameborder="0" class="' + value + '"></iframe>');
                 context.invoke("editor.saveRange");
-                context.invoke("editor.restoreRange");
+                //context.invoke("editor.restoreRange");
                 context.invoke("editor.focus");
                 context.invoke("editor.insertNode", img[0]);
                 //context.invoke("editor.insertText", '<iframe frameborder="0" class="' + value + '"></iframe>');
